4af86041df235f6f51ed948872bb1516beb1aebd
abhay9656 <abhay9656gupta@gmail.com>
2024-06-28 22:02:34 +0530

    Update LoginScreen component with input fields and login button

--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -1,25 +1,49 @@
-import React from 'react'
-import {StyleSheet, View,Text } from 'react-native'
+import React from "react";
+import { StyleSheet, View, Text, TextInput, Button } from "react-native";
 
 const LoginScreen = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Login</Text>
+      <TextInput
+        placeholder="Enter your name"
+        style={{
+          backgroundColor: "white",
+          margin: 10,
+          padding: 10,
+          borderRadius: 10,
+        }}
+      />
+      <TextInput style={styles.input} placeholder="Enter your password" />
+      <Button
+        title="Login"
+        color=""
+        style={{
+          width: "100px",
+          borderRadius: "5px",
+        }}
+      />
     </View>
-  )
-}
+  );
+};
 const styles = StyleSheet.create({
-    container:{
-       paddingVertical:50,
-       backgroundColor:'green',
-       flex:1,
-    },
-    text:{
-      textAlign:"center",
-      fontSize:40,
-      color:'white',
-      fontWeight:'bold',
-      marginTop:50
-    }
-})
-export default LoginScreen
+  container: {
+    paddingVertical: 50,
+    backgroundColor: "green",
+    flex: 1,
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 40,
+    color: "white",
+    fontWeight: "bold",
+    marginTop: 50,
+  },
+  input: {
+    backgroundColor: "white",
+    margin: 10,
+    padding: 10,
+    borderRadius: 10,
+  },
+});
+export default LoginScreen;
